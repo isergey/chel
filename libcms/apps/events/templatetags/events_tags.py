8194b49dd4a308bf3716f60671c183ef81e87b23
Sergey <dostovalov@gmail.com>
2012-03-26 14:03:15 +0400

    some fix

--- libcms/apps/events/templatetags/events_tags.py
+++ libcms/apps/events/templatetags/events_tags.py
@@ -7,12 +7,9 @@ from events.frontend.forms import CalendarFilterForm, get_current_month_choice, 
 
 register = template.Library()
 
-@register.inclusion_tag('events/tags/events_calendar.html',takes_context=True)
-def events_calendar(context,y=0, m=0):
-
-
+@register.inclusion_tag('events/tags/events_calendar.html', takes_context=True)
+def events_calendar(context, y=0, m=0):
     request = context['request']
-
 
     if request.method == 'POST':
         form = CalendarFilterForm(request.POST)
@@ -20,8 +17,8 @@ def events_calendar(context,y=0, m=0):
             y = int(form.cleaned_data['year'])
             m = int(form.cleaned_data['month'])
     else:
-        form = CalendarFilterForm(initial={'month':get_current_month_choice(),
-                                           'year': get_current_year_choice() })
+        form = CalendarFilterForm(initial={'month': get_current_month_choice(),
+                                           'year': get_current_year_choice()})
     today = date.today()
     year = today.year
     month = today.month
@@ -46,12 +43,12 @@ def events_calendar(context,y=0, m=0):
                 if event.start_date.day == day:
                     day_events['events'].append({
                         'id': event.id,
-#                        'title': event.title,
-#                        'teaser': event.teaser
+                        #                        'title': event.title,
+                        #                        'teaser': event.teaser
                     })
             week_events.append(day_events)
         calendar_of_events.append(week_events)
     return {'calendar': calendar_of_events,
             'month': month,
             'year': year,
-            'form':form}
+            'form': form}
