e48aa642b273a4fadd0703d8c7d51e66d10c0e7b
Sergey <dostovalov@gmail.com>
2012-05-03 17:10:04 +0400

    improve index app

--- libcms/apps/index/frontend/views.py
+++ libcms/apps/index/frontend/views.py
@@ -2,4 +2,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'frontend_base.html')
+    return render(request, 'index/frontend/index.html')
--- libcms/apps/index/urls.py
+++ libcms/apps/index/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('',
     (r'^', include('index.frontend.urls', namespace='frontend')),
