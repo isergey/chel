5918f9ac53fed509345de18b8e6c9de76fcce20e
Sergey <dostovalov@gmail.com>
2012-05-17 15:47:18 +0400

    menu tag cache

--- libcms/apps/menu/templatetags/menu_tags.py
+++ libcms/apps/menu/templatetags/menu_tags.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.core.cache import cache
 from django import template
 from django.utils import translation
 from django.utils.translation import to_locale, get_language
@@ -7,17 +8,29 @@ from menu.models import Menu, MenuItemTitle
 register = template.Library()
 @register.inclusion_tag('menu/tags/drow_menu.html', takes_context=True)
 def drow_menu(context, menu_slug):
-    path = context['request'].META['PATH_INFO']
-    try:
-        menu = Menu.objects.get(slug=menu_slug)
-    except Menu.DoesNotExist:
-        return ({
-            'menu': None
-        })
-
-    nodes = list(menu.root_item.get_descendants())
     lang=get_language()[:2]
-    item_titles = MenuItemTitle.objects.filter(item__in=nodes, lang=lang)
+    menu = cache.get('menu_' + menu_slug + lang, None)
+    if not menu:
+        try:
+            menu = Menu.objects.get(slug=menu_slug)
+            cache.set('menu_' + menu_slug + lang, menu)
+        except Menu.DoesNotExist:
+            return ({
+                'menu': None
+            })
+
+    path = context['request'].META['PATH_INFO']
+
+    nodes = cache.get('menu_nodes' + menu_slug + lang, None)
+    if not nodes:
+        nodes = list(menu.root_item.get_descendants())
+        cache.set('menu_nodes' + menu_slug + lang, nodes)
+
+    item_titles = cache.get('menu_item_titles' + menu_slug + lang, None)
+    if not item_titles:
+        item_titles = MenuItemTitle.objects.filter(item__in=nodes, lang=lang)
+        cache.set('menu_item_titles' + menu_slug + lang, item_titles)
+
     nd = {}
     for node in nodes:
         nd[node.id] = node
