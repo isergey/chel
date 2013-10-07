ac864a5ec98784495a95989660f9f212bbdaa6ae
Sergey <you@example.com>
2013-10-07 14:11:39 +0400

    rbooks

--- libcms/apps/rbooks/frontend/forms.py
+++ libcms/apps/rbooks/frontend/forms.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from ..models import Bookmarc
-
-class BookmarcForm(forms.ModelForm):
-    gen_id = forms.CharField(widget=forms.HiddenInput, max_length=32)
-    book_id = forms.CharField(widget=forms.HiddenInput, max_length=64)
-    comments = forms.CharField(widget=forms.Textarea, label=u'Комментарии', required=False)
-    page_number = forms.IntegerField(widget=forms.HiddenInput())
-    position_x = forms.FloatField(widget=forms.HiddenInput())
-    position_y = forms.FloatField(widget=forms.HiddenInput())
-    class Meta:
-        model = Bookmarc
-        exclude = ['user']
+# from ..models import Bookmarc
+#
+# class BookmarcForm(forms.ModelForm):
+#     gen_id = forms.CharField(widget=forms.HiddenInput, max_length=32)
+#     book_id = forms.CharField(widget=forms.HiddenInput, max_length=64)
+#     comments = forms.CharField(widget=forms.Textarea, label=u'Комментарии', required=False)
+#     page_number = forms.IntegerField(widget=forms.HiddenInput())
+#     position_x = forms.FloatField(widget=forms.HiddenInput())
+#     position_y = forms.FloatField(widget=forms.HiddenInput())
+#     class Meta:
+#         model = Bookmarc
+#         exclude = ['user']
