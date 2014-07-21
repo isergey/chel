e094d895b97895e0578022455c5d6ed98802a6be
Sergey <you@example.com>
2014-07-21 13:36:48 +0400

    rbooks

--- libcms/apps/pages/models.py
+++ libcms/apps/pages/models.py
@@ -53,7 +53,6 @@ class Page(MPTTModel):
         return translated ancestors
         """
         ancestors = list(self.get_ancestors())
-        print ancestors
         lang=get_language()[:2]
         ad = {}
         for ancestor in ancestors:
