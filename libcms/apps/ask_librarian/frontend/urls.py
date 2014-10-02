4234bed4be0c8e7178ca71124e701056feebe89d
Sergey Dostovalov <dostovalov@gmail.com>
2014-10-02 16:23:45 +0400

    init

--- libcms/apps/ask_librarian/frontend/urls.py
+++ libcms/apps/ask_librarian/frontend/urls.py
@@ -4,8 +4,9 @@ import views
 urlpatterns = patterns(views,
     url(r'^$', views.index , name="index"),
     url(r'^ask$', views.ask , name="ask"),
-    url(r'^detail/(?P<id>\d+)/', views.detail , name="detail"),
-    url(r'^detail/print/(?P<id>\d+)/', views.printed_detail , name="printed_detail"),
+    url(r'^detail/(?P<id>\d+)/', views.detail, name="detail"),
+    url(r'^detail/print/(?P<id>\d+)/', views.printed_detail, name="printed_detail"),
+    url(r'^my/', views.my_questions , name="my_questions"),
 #    url(r'^detail/', views.detail , name="detail"),
 #    url(r'^(?P<slug>[/_\-0-9A-Za-z]+)/$', views.show , name="show"),
 )
