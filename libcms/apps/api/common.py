10993a163d733e43d7b48870cec5b7f7dba216e4
Sergey Dostovalov <dostovalov@gmail.com>
2014-11-11 12:01:38 +0300

    mimetype

--- libcms/apps/api/common.py
+++ libcms/apps/api/common.py
@@ -7,6 +7,6 @@ from django.http import HttpResponse
 def response(data={}):
     if not isinstance(data, dict):
         raise TypeError(u'data must be dict type object')
-    return HttpResponse(simplejson.dumps(data, ensure_ascii=False), mimetype='application/json; charset=utf8')
+    return HttpResponse(simplejson.dumps(data, ensure_ascii=False), content_type='application/json; charset=utf8')
 
 
