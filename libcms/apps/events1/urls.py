10c56061d7c77ccccd457ad6d705770b94dd8280
Sergey Dostovalov <dostovalov@gmail.com>
2017-04-28 14:15:12 +0300

    events

--- libcms/apps/events1/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import *
-
-urlpatterns = patterns('',
-    (r'^admin/', include('events.administration.urls', namespace='administration')),
-    (r'^', include('events.frontend.urls', namespace='frontend')),
-
-)
-
