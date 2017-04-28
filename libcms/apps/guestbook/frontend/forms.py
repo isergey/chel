dad9e56badb480f3b09c98b9bf0fb92b13f37467
Sergey Dostovalov <dostovalov@gmail.com>
2017-04-28 14:36:57 +0300

    captcha

--- libcms/apps/guestbook/frontend/forms.py
+++ libcms/apps/guestbook/frontend/forms.py
@@ -7,7 +7,7 @@ from ..models import Feedback
 
 class FeedbackForm(forms.ModelForm):
     content = forms.CharField(widget=forms.Textarea, label=u'Текст отзыва')
-    captcha = CaptchaField(label=u'Введите текст изображенный на картинке')
+    # captcha = CaptchaField(label=u'Введите текст изображенный на картинке')
     class Meta:
         model = Feedback
         exclude = ('comment', 'publicated')
