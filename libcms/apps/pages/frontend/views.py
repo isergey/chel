fe12f3d25e36f1d58ee9777bd5628df4f0b29aca
Sergey Dostovalov <dostovalov@gmail.com>
2018-07-19 13:17:49 +0300

    children pages

--- libcms/apps/pages/frontend/views.py
+++ libcms/apps/pages/frontend/views.py
@@ -43,16 +43,16 @@ def show(request, slug):
         content = None
     children = None
 
-    #    if not page.is_leaf_node():
-    #        children = list(Page.objects.filter(parent=page, public=True))
-    #        contents = Content.objects.filter(page__in=children, lang=cur_language[:2])
-    #        cd = {}
-    #        for child in children:
-    #            cd[child.id] = child
-    #
-    #        for contend_page in contents:
-    #            if contend_page.page_id in cd:
-    #                cd[contend_page.page_id].content = contend_page
+    if not page.is_leaf_node():
+        children = list(Page.objects.filter(parent=page, public=True))
+        contents = Content.objects.filter(page__in=children, lang=cur_language[:2])
+        cd = {}
+        for child in children:
+           cd[child.id] = child
+
+        for contend_page in contents:
+           if contend_page.page_id in cd:
+               cd[contend_page.page_id].content = contend_page
 
     user = request.user
     log = ViewLog(page=page)
@@ -64,6 +64,7 @@ def show(request, slug):
     return render(request, 'pages/frontend/show.html', {
         'page': page,
         'content': content,
+        'children': children,
     })
 
 
