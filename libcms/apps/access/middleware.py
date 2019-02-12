6084e7fa11592236248133474bcd6839d2a269e1
Sergey Dostovalov <dostovalov@gmail.com>
2019-02-12 15:34:42 +0300

    Запрет ботов

--- libcms/apps/access/middleware.py
+++ libcms/apps/access/middleware.py
@@ -3,6 +3,24 @@ from django.shortcuts import HttpResponse
 
 DEFAULT_DISALLOW_BOTS = [
     'ahrefsbot',
+    'altavista', 'slurp', 'blackwidow', 'chinaclaw', 'custo', 'disco', 'download', 'demon',
+    'ecatch',
+    'eirgrabber', 'emailsiphon', 'emailwolf', 'superhttp', 'surfbot', 'webwhacker',
+    'express', 'webpictures', 'extractorpro', 'eyenetie', 'flashget', 'getright', 'getweb!', 'go!zilla',
+    'go-ahead-got-it',
+    'grabnet', 'grafula', 'hmview', 'go!zilla', 'go-ahead-got-it',
+    'rafula', 'hmview', 'httrack', 'stripper', 'sucker', 'indy', 'interget',
+    'ninja', 'jetcar', 'spider', 'larbin',
+    'leechftp', 'downloader', 'tool', 'navroad', 'nearsite', 'netants', 'takeout', 'wwwoffle',
+    'grabnet', 'netspider', 'vampire',
+    'netzip', 'octopus', 'offline', 'pagegrabber', 'foto', 'pavuk', 'pcbrowser',
+    'realdownload', 'reget', 'sitesnagger', 'smartdownload', 'superbot', 'webspider',
+    'teleport', 'voideye', 'collector', 'webauto',
+    'webcopier', 'webfetch', 'webgo', 'webleacher', 'webreaper',
+    'websauger', 'extractor', 'quester', 'webstripper', 'webzip', 'wget', 'widow', 'zeus',
+    'twengabot', 'htmlparser', 'libwww',
+    'scan', 'pyth',
+    'pyq', 'webcollector', 'webcopy', 'webcraw'
 ]
 
 DISALLOW_BOTS = getattr(settings, 'ACCESS_DISALLOW_BOTS', DEFAULT_DISALLOW_BOTS)
