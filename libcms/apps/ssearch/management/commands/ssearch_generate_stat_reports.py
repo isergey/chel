7815def58b3f379155ba321ca1f28d3774aa00e2
Sergey Dostovalov <dostovalov@gmail.com>
2020-03-04 21:54:05 +0300

    статистика

--- libcms/apps/ssearch/management/commands/ssearch_generate_stat_reports.py
+++ libcms/apps/ssearch/management/commands/ssearch_generate_stat_reports.py
@@ -1,6 +1,6 @@
 import fasteners
 from django.core.management.base import BaseCommand, CommandError
-from ssearch.statistics.views import generate_incomes_report, generate_actions_report, get_users_report_file_path
+from ssearch.statistics.views import generate_incomes_report, generate_actions_report, generate_users_report
 
 
 class Command(BaseCommand):
@@ -10,12 +10,12 @@ class Command(BaseCommand):
         gotten = a_lock.acquire(blocking=False)
         try:
             if gotten:
-                print 'generate_incomes_report'
-                generate_incomes_report()
-                print 'generate_actions_report'
-                generate_actions_report()
-                print 'get_users_report_file_path'
-                get_users_report_file_path()
+                # print 'generate_incomes_report'
+                # generate_incomes_report()
+                # print 'generate_actions_report'
+                # generate_actions_report()
+                print 'generate_users_report'
+                generate_users_report()
         finally:
             if gotten:
                 a_lock.release()
