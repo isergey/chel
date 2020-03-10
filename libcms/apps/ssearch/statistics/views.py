aa8118e6d7ac158c910fe939092699ffa40729ed
Sergey Dostovalov <dostovalov@gmail.com>
2020-03-10 18:37:28 +0300

    статистика

--- libcms/apps/ssearch/statistics/views.py
+++ libcms/apps/ssearch/statistics/views.py
@@ -215,6 +215,7 @@ def _get_detail_log():
     for i, detail_log in enumerate(models.DetailLog.objects.all().iterator()):
         if i % 1000 == 0:
             print i, len(cache.keys())
+        continue
         if detail_log.record_id in cache:
             record_content = cache.get(detail_log.record_id)
             if record_content is None:
