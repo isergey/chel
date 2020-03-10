f1a3c9eddad52371634a115509c90145bc3e01a6
Sergey Dostovalov <dostovalov@gmail.com>
2020-03-10 18:48:09 +0300

    статистика

--- libcms/apps/ssearch/statistics/views.py
+++ libcms/apps/ssearch/statistics/views.py
@@ -220,7 +220,7 @@ def _get_detail_log():
             content = cache.get(detail_log.record_id)
             if content is None:
                 continue
-            print 'from cache', detail_log.record_id
+            # print 'from cache', detail_log.record_id
             yield detail_log, content
             continue
 
@@ -228,10 +228,10 @@ def _get_detail_log():
             record_content = models.RecordContent.objects.using(models.RECORDS_DB_CONNECTION).get(record_id=detail_log.record_id)
             content = record_content.unpack_content()
             cache[detail_log.record_id] = content
-            print 'set cache'
+            # print 'set cache'
             yield detail_log, content
         except models.RecordContent.DoesNotExist:
-            print 'not found'
+            # print 'not found'
             cache[detail_log.record_id] = None
     #     record_ids.append(dict(detail_log=detail_log, record_content=None))
     #     if len(record_ids) > 20:
