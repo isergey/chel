defb44b7aed0bf99a0ec08f6ab81ff8c1ca66e28
Sergey Dostovalov <dostovalov@gmail.com>
2020-03-12 17:34:34 +0300

    статистика

--- libcms/apps/ssearch/models.py
+++ libcms/apps/ssearch/models.py
@@ -337,7 +337,6 @@ def log_search_request(params, user=None, total=0, in_results=False, session_id=
 
     SearchLog.objects.bulk_create([search_log])
 
-    print 'created'
 
 def log_detail(record_id, user=None, action=0, session_id=''):
     if not record_id:
