a13ff7676885fcaf13220b3c0808671656378d5c
Sergey Dostovalov <dostovalov@gmail.com>
2020-03-12 17:42:53 +0300

    статистика

--- libcms/apps/ssearch/frontend/views.py
+++ libcms/apps/ssearch/frontend/views.py
@@ -488,19 +488,17 @@ def index(request, catalog='uc'):
     #         make_logging = False
 
     session_id = _get_session_id(request)
-    try:
-        make_logging = not _is_request_from_detail(request)
-        user = request.user if request.user.is_authenticated else None
-        if make_logging:
-            models.log_search_request(
-                kv_dicts,
-                user=user,
-                total=result.count(),
-                in_results=len(kv_dicts) > 1,
-                session_id=session_id
-            )
-    except Exception as e:
-        print e
+    make_logging = not _is_request_from_detail(request)
+    user = request.user if request.user.is_authenticated else None
+    if make_logging:
+        models.log_search_request(
+            params=_flat_kv_args(kv_dicts),
+            user=user,
+            total=result.count(),
+            in_results=len(kv_dicts) > 1,
+            session_id=session_id
+        )
+
     response = render(request, 'ssearch/frontend/index.html', {
         'records': records,
         'facets': facets,
