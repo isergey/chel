b087b99402e3de64df60d03adb4ef5a6dc5348cc
Sergey Dostovalov <dostovalov@gmail.com>
2020-03-13 15:26:57 +0300

    статистика

--- libcms/apps/ssearch/frontend/views.py
+++ libcms/apps/ssearch/frontend/views.py
@@ -491,7 +491,7 @@ def index(request, catalog='uc'):
     session_id = _get_session_id(request)
     make_logging = not _is_request_from_detail(request)
     user = request.user if request.user.is_authenticated else None
-    # make_logging = False
+    make_logging = False
     if make_logging:
         models.log_search_request(
             params=_flat_kv_args(kv_dicts),
