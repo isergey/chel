4ab40e74cc7031c420785c9fe4865e44bcb11bcc
Sergey Dostovalov <dostovalov@gmail.com>
2020-04-13 22:18:48 +0300

    события

--- libcms/apps/events/templatetags/events_tags.py
+++ libcms/apps/events/templatetags/events_tags.py
@@ -29,7 +29,7 @@ def events_calendar(context, y=0, m=0):
 
     if y: year = y
     if m: month = m
-    weeks = calendar.monthcalendar(year, month)
+
     cache_key = 'events_y_m' + str(year) + str(month) + 'active=1'
 
     month_range = calendar.monthrange(year, month)
@@ -37,13 +37,14 @@ def events_calendar(context, y=0, m=0):
     end = timezone.datetime(year, month, month_range[1], 0, 0, 0)
     q = Q(active=True) & Q(Q(start_date__lte=start) | Q(start_date__lte=end)) & Q(end_date__gte=start)
     # events = Event.objects.filter(active=True, start_date__lte=start, end_date__gte=end)
-    events = Event.objects.filter(q)
-
-    if not events:
-        events = list(events)
-        cache.set(cache_key, events)
+    events = Event.objects.filter(q).values('')
+    # if not events:
+    #     events = list(events)
+    #     cache.set(cache_key, events)
 
     calendar_of_events = []
+
+    weeks = calendar.monthcalendar(year, month)
     for week in weeks:
         week_events = []
         for day in week:
@@ -55,19 +56,19 @@ def events_calendar(context, y=0, m=0):
             day_events['day'] = day
             if day == today.day and year == today.year and month == today.month:
                 day_events['today'] = True
-            # for event in events:
-            #     if day == 0: continue
-            #     date_for_day_start = timezone.datetime(year, month, day, 0, 0, 0)
-            #     date_for_day_end = timezone.datetime(year, month, day, 23, 59, 59)
-            #     if ( event.start_date <= date_for_day_start or event.start_date <= date_for_day_end ) and event.end_date >= date_for_day_start:
-            #         day_events['events'].append({
-            #             'id': event.id,
-            #             #                        'title': event.title,
-            #             #                        'teaser': event.teaser
-            #         })
+            for event in events:
+                if day == 0: continue
+                date_for_day_start = timezone.datetime(year, month, day, 0, 0, 0)
+                date_for_day_end = timezone.datetime(year, month, day, 23, 59, 59)
+                if ( event.start_date <= date_for_day_start or event.start_date <= date_for_day_end ) and event.end_date >= date_for_day_start:
+                    day_events['events'].append({
+                        'id': event.id,
+                        #                        'title': event.title,
+                        #                        'teaser': event.teaser
+                    })
             week_events.append(day_events)
         calendar_of_events.append(week_events)
-    # print calendar_of_events
+    print(calendar_of_events)
     return {
         'calendar': calendar_of_events,
         'month': month,
