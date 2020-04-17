127ba618960cfca6bd4dc98fe7c27c5038761f25
Sergey Dostovalov <dostovalov@gmail.com>
2020-04-17 17:13:19 +0300

    обн

--- libcms/apps/contact_form/frontend/forms.py
+++ libcms/apps/contact_form/frontend/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .. import models
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV3
 
 
 class ContactRequestForm(forms.ModelForm):
+    captcha = ReCaptchaField(label='', widget=ReCaptchaV3(
+        attrs={
+            'required_score': 0.85,
+        }
+    ))
+
     class Meta:
         model = models.ContactRequest
         fields = ['name', 'email', 'phone', 'message']
