521f013dda9454bc40a6f623ea8fedd091e89025
Sergey Dostovalov <dostovalov@gmail.com>
2020-05-27 16:38:58 +0300

    upd

--- libcms/apps/rbooks/models.py
+++ libcms/apps/rbooks/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class ViewLog(models.Model):
     doc_id = models.CharField(verbose_name='Идентификатор документа', db_index=True, max_length=32)
-    collection = models.CharField(verbose_name='Коллекция', db_index=True, max_length=64, blank=True)
+    collection = models.CharField(verbose_name='Коллекция', db_index=True, max_length=256, blank=True)
     view_dt = models.DateTimeField(verbose_name='Время просмотра', db_index=True,auto_now_add=True)
     user_id = models.BigIntegerField(verbose_name='Пользователь', db_index=True, default=-1)
 
