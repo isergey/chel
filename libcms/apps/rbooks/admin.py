cb58d8187a647ed7c71c467accd80eef8ef545f6
sergey <dostovalov@gmail.com>
2022-04-06 13:21:22 +0300

    rbooks 2

--- libcms/apps/rbooks/admin.py
+++ libcms/apps/rbooks/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from . import models
 
+
 class ViewLogAdmin(admin.ModelAdmin):
     list_display = ('doc_id', 'collection', 'view_dt', 'user_id')
+
 
 admin.site.register(models.ViewLog, ViewLogAdmin)
