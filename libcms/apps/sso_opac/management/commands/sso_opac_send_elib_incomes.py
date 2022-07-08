94d6d7b84441879c048c0de0dea47b4bfc1ff420
sergey <dostovalov@gmail.com>
2022-07-08 20:01:05 +0300

    elib income schedule

--- libcms/apps/sso_opac/management/commands/sso_opac_send_elib_incomes.py
+++ libcms/apps/sso_opac/management/commands/sso_opac_send_elib_incomes.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         now = datetime.now()
         monthrange = calendar.monthrange(now.year, now.month)
-        print(monthrange)
+
         if now.day == 15:
             from_date = datetime(year=now.year, month=now.month, day=1)
         elif now.day == monthrange[1]:
