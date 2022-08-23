b686825ec29730d5bee338522a5ce98da3193f82
sergey <dostovalov@gmail.com>
2022-08-23 17:44:51 +0300

    seo optimisation

--- libcms/apps/ssearch/sitemap/views.py
+++ libcms/apps/ssearch/sitemap/views.py
@@ -19,8 +19,8 @@ def index(request):
 
 def records(request, offset):
     offset = int(offset) * LIMIT
-    record_models = models.Record.objects.values('id', 'update_date').all().order_by('created_date')[
-                    offset:offset + LIMIT]
+    record_models = list(models.Record.objects.values('id', 'update_date').all().order_by('created_date')[
+                    offset:offset + LIMIT])
     return render(request, 'ssearch/sitemap/index.html', {
         'records': record_models
     }, content_type='application/xml')
