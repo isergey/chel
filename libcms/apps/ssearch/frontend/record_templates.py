cb049f3760f62b3f25a64c9812ba0baee7b50a3e
sergey <dostovalov@gmail.com>
2022-08-23 20:22:08 +0300

    seo optimisation

--- libcms/apps/ssearch/frontend/record_templates.py
+++ libcms/apps/ssearch/frontend/record_templates.py
@@ -174,8 +174,8 @@ class RusmarcTemplate(object):
     def __get_title(self, field=None):
         title = []
         title200 = get_title(self.rq, self.rq.get_field('200'))
-        title461 = get_title(self.rq, self.rq.get_field('461'))
-        title463 = get_title(self.rq, self.rq.get_field('463'))
+        title461 = get_title(self.rq, self.rq.get_field('461').get_field('200'))
+        title463 = get_title(self.rq, self.rq.get_field('463').get_field('200'))
 
         if title461 and title463:
             title.append(title200)
@@ -189,7 +189,7 @@ class RusmarcTemplate(object):
             append_symbol('.', title)
             title.append(' — ')
             title.append(title200)
-        elif title461:
+        elif title463:
             title.append(title461)
             append_symbol('.', title)
             title.append(' — ')
