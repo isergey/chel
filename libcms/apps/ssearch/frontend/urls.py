2297122a13932bbb9b2b26dab1d48d47ab4c90ed
sergey <dostovalov@gmail.com>
2022-08-23 18:12:46 +0300

    seo optimisation

--- libcms/apps/ssearch/frontend/urls.py
+++ libcms/apps/ssearch/frontend/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import re_path
 from . import views
 
 urlpatterns = (
-    re_path(r'^$', views.index, name="index"),
+    re_path(r'^results$', views.index, name="index"),
     re_path(r'^detail$', views.detail, name="detail"),
     re_path(r'^log$', views.log, name="log"),
     re_path(r'^m_f$', views.more_facet, name="more_facet"),
