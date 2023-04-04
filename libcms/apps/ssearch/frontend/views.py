f277a82f7ed9cf4bd0939af0f51199f3c862963b
sergey <dostovalov@gmail.com>
2023-04-04 15:12:06 +0300

    collections

--- libcms/apps/ssearch/frontend/views.py
+++ libcms/apps/ssearch/frontend/views.py
@@ -348,7 +348,7 @@ def build_pivot_tree(pivot):
 
 def collections():
     # pivot_collections = 'collection2_s,collection_s,collection3_s,collection4_s,collection5_s'
-    pivot_collections = 'collection2_s,collection_s,collection3_s,collection4_s,collection5_s'
+    pivot_collections = 'collection2_s,collection_s,collection3_s,collection4_s'
 
     uc = init_solr_collection('uc')
     faset_params = FacetParams()
