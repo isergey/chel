f8c9093e7c48dc2a9c00c10072cc1932ae649709
Sergey <dostovalov@gmail.com>
2025-03-17 16:06:34 +0300

    export

--- libcms/apps/export/management/commands/export_search_log.py
+++ libcms/apps/export/management/commands/export_search_log.py
@@ -6,17 +6,25 @@ from ssearch import models as search_models
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        for log in search_models.SearchLog.objects.all()[0: 5000000].iterator():
-            log: search_models.SearchLog = log
-            data = {
-                'id': log.id,
-                'user': log.user_id,
-                'params': log.params,
-                'total': log.total,
-                'in_results': log.in_results,
-                'session_id': log.session_id,
-                'date_time': log.date_time.isoformat(),
-                'params_crc32': log.params_crc32,
-            }
+        offset = 0
+        while True:
+            limit = offset + 100000
+            count = 0
+            for log in search_models.SearchLog.objects.all()[offset: limit].iterator():
+                count += 1
+                log: search_models.SearchLog = log
+                data = {
+                    'id': log.id,
+                    'user': log.user_id,
+                    'params': log.params,
+                    'total': log.total,
+                    'in_results': log.in_results,
+                    'session_id': log.session_id,
+                    'date_time': log.date_time.isoformat(),
+                    'params_crc32': log.params_crc32,
+                }
 
-            print(json.dumps(data, ensure_ascii=False))
+                print(json.dumps(data, ensure_ascii=False))
+
+            if count == 0:
+                break
