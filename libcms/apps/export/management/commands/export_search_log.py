8d67be1cb6f783d9b70de4ecb571f1f75e443750
Sergey <dostovalov@gmail.com>
2025-03-17 15:22:17 +0300

    export

---
+++ libcms/apps/export/management/commands/export_search_log.py
@@ -0,0 +1,21 @@
+import json
+from ssearch import models as search_models
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for log in search_models.SearchLog.objects.all().iterator():
+            log: search_models.SearchLog = log
+            data = {
+                'id': log.id,
+                'user': log.user_id,
+                'params': log.params,
+                'total': log.total,
+                'in_results': log.in_results,
+                'session_id': log.session_id,
+                'date_time': log.date_time.isoformat(),
+                'params_crc32': log.params_crc32,
+            }
+
+            print(json.dumps(data, ensure_ascii=False))
