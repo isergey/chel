abb26742bca83de9ace718f9f91c9c39156331e4
Sergey <dostovalov@gmail.com>
2025-04-25 13:05:20 +0300

    delete file

--- libcms/apps/filebrowser/administration/urls.py
+++ libcms/apps/filebrowser/administration/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = (
     url(r'^$', views.index, name="index"),
     url(r'^upload_file/$', views.upload_file, name="upload_file"),
-    url(r'^delete/$', views.delete, name="delete"),
+    url(r'^delete/(?P<file_id>\d+)/$', views.delete, name="delete"),
     url(r'^ajax_file_info/$', views.ajax_file_info,  name="ajax_file_info"),
 )
